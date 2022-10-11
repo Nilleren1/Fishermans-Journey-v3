921ca7532202e435a01bf476ea4eb266494813ba
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-10-11 22:08:09 +0200

    FishingMiniGame
    FIx til minigame i hver scene

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,25 +1,25 @@
 using System;
 using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 using UnityEngine;
+
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
-    [SerializeField]
-    private float jumpForce;
-    [SerializeField]
-    private float sprintSpeed;
-    [SerializeField]
-    public LayerMask groundLayer;
-    [SerializeField]
-    private LayerMask wallLayer;
+    public GameObject MiniGame;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] public LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D body;
     private Animator anim;
     private CapsuleCollider2D capsuleCollider;
     private float wallJumpCooldown;
     private float horizontalInput;
+    private bool PlayerIsFishing;
 
     private void Awake()
     {
@@ -38,14 +38,22 @@ public class PlayerMovement : MonoBehaviour
         else if (body.velocity.x > 0 || body.velocity.y > 0)
         {
             anim.SetBool("Fishing", false);
+            PlayerIsFishing = false;
+
         }
-        
-            
-        
+
+        if (PlayerIsFishing == false)
+        {
+            hideMiniGame();
+        }
+        else
+        {
+            showMiniGame();
+        }
+
 
         horizontalInput = Input.GetAxis("Horizontal");
 
-        
 
         //Flip player when moving left or right
         if (horizontalInput > 0.01f)
@@ -61,28 +69,29 @@ public class PlayerMovement : MonoBehaviour
         anim.SetBool("Walk", horizontalInput != 0);
         anim.SetBool("Sprint", Input.GetKey(KeyCode.LeftShift) == true && isGrounded() == true);
         anim.SetBool("Grounded", isGrounded());
-        
+
 
         //Wall Jump logic
         if (wallJumpCooldown > 0.2f)
         {
-            
+
 
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-                if (onWall() && !isGrounded())
-                {
-                    body.gravityScale = 0;
-                    body.velocity = Vector2.zero;
-                }
-                else
-                {
-                    body.gravityScale = 3;
-                }
-                if (Input.GetKey(KeyCode.Space))
-                {
-                    Jump();
-                }
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+            }
+            else
+            {
+                body.gravityScale = 3;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
 
         }
         else
@@ -111,14 +120,16 @@ public class PlayerMovement : MonoBehaviour
             {
                 body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 9, 0);
                 //Flip the character on wall jump
-                transform.localScale = new Vector3(-MathF.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                transform.localScale = new Vector3(-MathF.Sign(transform.localScale.x), transform.localScale.y,
+                    transform.localScale.z);
             }
             else
             {
                 body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 4, 8);
             }
+
             wallJumpCooldown = 0;
-            
+
         }
     }
 
@@ -132,7 +143,8 @@ public class PlayerMovement : MonoBehaviour
     private void Fish()
     {
         anim.SetBool("Fishing", true);
-        
+        PlayerIsFishing = true;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -146,7 +158,8 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded()
     {
         RaycastHit2D raycastHit =
-            Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, groundLayer);
+            Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0,
+                new Vector2(transform.localScale.x, 0), 0.1f, groundLayer);
         //returns true or false whether player is grounded or not.
         return raycastHit.collider != null;
     }
@@ -155,8 +168,21 @@ public class PlayerMovement : MonoBehaviour
     private bool onWall()
     {
         RaycastHit2D raycastHit =
-            Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0, Vector2.down, 0.1f, wallLayer);
+            Physics2D.BoxCast(capsuleCollider.bounds.center, capsuleCollider.bounds.size, 0, Vector2.down, 0.1f,
+                wallLayer);
         //returns true or false whether player is grounded or not.
         return raycastHit.collider != null;
     }
+
+    public void hideMiniGame()
+    {
+        MiniGame.gameObject.SetActive(false);
+    }
+
+    public void showMiniGame()
+    {
+        MiniGame.gameObject.SetActive(true);
+    }
 }
+
+    
