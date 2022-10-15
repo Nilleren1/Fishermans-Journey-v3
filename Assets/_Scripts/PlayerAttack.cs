bbf40646c209e2135b5d9005f77bac4aa7da1c55
Nilleren1 <nicolai.hoegsbjerg@gmail.com>
2022-10-15 16:46:02 +0200

    Attack Animations
    Har arbejdet lidt på noget attack, mangler stadig noget logic til at registrerer at man rammer en fjende. Vil også gerne lave det om til combo istedet for to seperate knapper.

---
+++ Assets/_Scripts/PlayerAttack.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerAttack : MonoBehaviour
+{
+    
+    public Animator animator;
+
+    public Transform attackPoint;
+    [SerializeField] public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+
+    private void Awake()
+    {
+        
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //button = right mouse click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Attack();
+
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Attack2();
+
+        }
+
+    }
+
+    public void Attack()
+    {
+        //Play attack animation
+        animator.SetTrigger("Attack");
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        //Damage enemy
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
+    }
+    public void Attack2()
+    {
+        //Play attack animation
+        animator.SetTrigger("Attack2");
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        //Damage enemy
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+
+}
