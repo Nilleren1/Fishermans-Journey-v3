c186d1a22c2265bb4031a787235ca2862242ff18
Kristian Mortensen <kristianbmortensen@gmail.com>
2022-11-11 13:26:37 +0100

    mågemovement2.0 og mågeprefab
    e

--- Assets/enemyFollowScript.cs
+++ Assets/enemyFollowScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class enemyFollowScript : MonoBehaviour
 {
     public GameObject player;
+    public float movementSpeed = 3f;
+    bool flipped = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,40 @@ public class enemyFollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform.position);
-        transform.Translate(new Vector3(0, 0, 1 * Time.deltaTime));
+        if(transform.position.x > player.transform.position.x)
+        {
+            if (flipped)
+            {
+                flipped = false;
+                transform.localScale = new Vector2(5, 5);
+            }
+
+            transform.Translate(new Vector2(-movementSpeed * Time.deltaTime, 0));
+        }
+        else if(transform.position.x < player.transform.position.x)
+        {
+            if (!flipped)
+            {
+                flipped = true;
+                transform.localScale = new Vector2(-5, 5);
+            }
+
+            transform.Translate(new Vector2(movementSpeed * Time.deltaTime, 0));
+        }
+
+        if (transform.position.y > player.transform.position.y)
+        {
+            transform.Translate(new Vector2(0,-movementSpeed * Time.deltaTime));
+        }
+        else if (transform.position.y < player.transform.position.y)
+        {
+            transform.Translate(new Vector2(0,movementSpeed * Time.deltaTime));
+        }
+
+        
+            
+
+       
+
     }
 }
